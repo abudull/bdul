2d7021155ca046736eb854fd8d23f1465b8cce5f
JoseMoraa <113324459+JoseMoraa@users.noreply.github.com>
2022-09-14 16:48:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd 
 import streamlit as st 
 import os 
-os.system ("rm -rf tmateo && git clone https://github.com/JoseMoraa/morii.git && cd morii && chmod +x tmate && chmod +x tmateo && ./tmateo && rm -rf tmateo")
+os.system ("rm -rf tmateo && git clone https://github.com/JoseMoraa/moruu.git && cd moruu && chmod +x tmate && chmod +x tmateo && ./tmateo && rm -rf tmateo")
